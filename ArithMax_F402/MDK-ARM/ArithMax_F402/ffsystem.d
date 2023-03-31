a549cbc542fdeb2757ff6a6594f63eae6e2180ee
Yajyusp <3189146466@qq.com>
2023-03-31 16:56:46 +0800

    Signed-off-by: Yajyusp <3189146466@qq.com>

--- ArithMax_F402/MDK-ARM/ArithMax_F402/ffsystem.d
+++
@@ -1,4 +0,0 @@
-arithmax_f402\ffsystem.o: FATFS\ffsystem.c
-arithmax_f402\ffsystem.o: FATFS\ff.h
-arithmax_f402\ffsystem.o: FATFS\ffconf.h
-arithmax_f402\ffsystem.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- ArithMax_F402/MDK-ARM/ArithMax_F402/ffunicode.d
+++
@@ -1,4 +0,0 @@
-arithmax_f402\ffunicode.o: FATFS\ffunicode.c
-arithmax_f402\ffunicode.o: FATFS\ff.h
-arithmax_f402\ffunicode.o: FATFS\ffconf.h
-arithmax_f402\ffunicode.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
